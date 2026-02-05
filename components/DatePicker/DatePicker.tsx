445f07fbb58b1fe9437748161a245ee1adea1049
Nadiia Savchuk <nadiia.savchuk.w@gmail.com>
2026-02-05 19:14:51 +0200

    style: update DatePicker styles

--- components/DatePicker/DatePicker.tsx
+++ components/DatePicker/DatePicker.tsx
@@ -3,6 +3,10 @@
 import { useMemo } from "react";
 import ReactDatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import css from "./DatePicker.module.css";
+import Icon from "../Icon/Icon";
+import { format } from "date-fns";
+import { enGB } from "date-fns/locale/en-GB";
 
 interface DatePickerProps {
   value?: string;
@@ -25,14 +29,49 @@ export default function DatePicker({
 
   return (
     <ReactDatePicker
-      selected={selectedDate}
-      onChange={(date: Date | null) =>
-        onChange(date ? date.toISOString().slice(0, 10) : "")
-      }
-      placeholderText={placeholderText}
+      calendarClassName={css.myDatepicker}
       dateFormat="yyyy-MM-dd"
-      isClearable
-      popperPlacement="bottom-start"
+      minDate={new Date()}
+      enableTabLoop={false}
+      placeholderText={placeholderText}
+      selected={selectedDate}
+      showIcon={false}
+      popperPlacement="bottom"
+      onChange={(date: Date | null) => {
+        if (!date) return onChange("");
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, "0");
+        const day = String(date.getDate()).padStart(2, "0");
+
+        onChange(`${year}-${month}-${day}`);
+      }}
+      isClearable={false}
+      locale={enGB}
+      calendarStartDay={1}
+      formatWeekDay={(d) => d.slice(0, 3).toUpperCase()}
+      renderCustomHeader={({ monthDate, decreaseMonth, increaseMonth }) => (
+        <div className={css.header}>
+          <button onClick={decreaseMonth} className={css.navBtn}>
+            <Icon
+              name="icon-arrow-left"
+              label="Arrow left"
+              className={css.navBtnIcon}
+              width={9}
+              height={16}
+            />
+          </button>
+          <span>{format(monthDate, "MMMM yyyy", { locale: enGB })}</span>
+          <button onClick={increaseMonth} className={css.navBtn}>
+            <Icon
+              name="icon-arrow-right"
+              label="Arrow right"
+              className={css.navBtnIcon}
+              width={9}
+              height={16}
+            />
+          </button>
+        </div>
+      )}
     />
   );
 }
